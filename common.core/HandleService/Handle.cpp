3ef74d18199366ed71e6995157bc7086699e3fce
lenglengai <lenglengai@gmail.com>
2014-06-17 18:36:42 +0800

    HandleService

---
+++ common.core/HandleService/Handle.cpp
@@ -0,0 +1,59 @@
+#include "../DefInc.h"
+#include "Context.h"
+#include "Handle.h"
+
+namespace std {
+
+	void Handle::addContext(ContextPtr& nContext)
+	{
+		mContexts.push_back(nContext);
+	}
+
+	void Handle::runStart()
+	{
+		mThread.reset(new boost::thread(boost::bind(&Handle::runHandle, this)));
+	}
+
+	void Handle::runHandle()
+	{
+		mStop = false;
+		while (this->runInternal());
+		mStop = true;
+	}
+
+	void Handle::runStop()
+	{
+		mStop = true;
+	}
+
+	void Handle::runClear()
+	{
+		mContexts.clear();
+		mThread.reset();
+		mStop = true;
+	}
+
+	bool Handle::runInternal()
+	{
+		if (true == mStop) return false;
+		list<ContextPtr>::iterator it = mContexts.begin();
+		for (; it != mContexts.end(); ++it)
+		{
+			ContextPtr& context_ = (*it);
+			context_->runContext();
+		}
+		boost::this_thread::sleep(boost::posix_time::seconds(1));
+		return true;
+	}
+
+	Handle::Handle()
+	{
+		this.runClear();
+	}
+
+	Handle::~Handle()
+	{
+		this.runClear();
+	}
+
+}
