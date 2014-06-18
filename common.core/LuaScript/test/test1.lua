84d7830b466423c7b27b74bab5c29d7ab5eebade
lenglengai <lenglengai@gmail.com>
2014-06-18 11:14:28 +0800

    update

--- common.core/LuaScript/test/test1.lua
+++
@@ -1,8 +0,0 @@
-function test1(nMyBase)
-	local result = nMyBase:fun1(true)
-	print(result)
-	if 6 == result then
-		return true
-	end
-	return false
-end
--- common.core/LuaScript/test/test2.lua
+++
@@ -1,8 +0,0 @@
-function test2(nMySub)
-	local result = nMySub:fun0(2)
-	print(result)
-	if 5 == result then
-		return true
-	end
-	return false
-end
