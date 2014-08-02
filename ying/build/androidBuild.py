5c3c6ce16579bfcd34f69dd7f6abe412954a58ec
lenglengai <lenglengai@gmail.com>
2014-08-02 17:32:23 +0800

    android 15

--- ying/build/androidBuild.py
+++ ying/build/androidBuild.py
@@ -9,7 +9,7 @@ os.environ['WANMEI_INCLUDE'] = includePath
 cmdPath = os.path.abspath('../../mbclient')
 os.chdir(cmdPath)
 
-cmakeCmd = 'cocos compile -p android -j 4 --ap 20'
+cmakeCmd = 'cocos compile -p android -j 4 --ap 15'
 subprocess.call(cmakeCmd, shell=True)
 
 print 'finish build of android, byebye!'
